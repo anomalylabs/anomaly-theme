daa7f2ea6a1e8d30322d5dc33f280fcad264f2b3
Ryan Thompson <service@aiwebsystems.com>
2015-05-12 15:18:18 -0500

    Loader

--- resources/js/theme/initialize.js
+++ resources/js/theme/initialize.js
@@ -45,6 +45,6 @@ $(function () {
 
     // Clear remote modals when closed.
     $('.modal.remote').on('hidden.bs.modal', function () {
-        $(this).find('.modal-content').html('');
+        $(this).find('.modal-content').html('<div class="modal-loading"><div class="active loader"></div></div>');
     });
 });
