1ca6324ade740e37a831799d122df0ff3f59775d
Ryan Thompson <service@aiwebsystems.com>
2015-05-14 04:24:50 -0500

    Confirm

--- resources/js/theme/initialize.js
+++ resources/js/theme/initialize.js
@@ -47,4 +47,44 @@ $(function () {
     $('.modal.remote').on('hidden.bs.modal', function () {
         $(this).find('.modal-content').html('<div class="modal-loading"><div class="active loader"></div></div>');
     });
+
+    // Initialize confirm buttons.
+    $('[data-toggle="confirm"]').click(function (e) {
+
+        e.preventDefault();
+
+        bootbox.confirm($(this).data('message') || DEFAULT_CONFIRM_MESSAGE, function (result) {
+            if (result === true) {
+                $(e.target).unbind('click')[0].click();
+            }
+        });
+    });
+
+    // Initialize prompt buttons.
+    $('[data-toggle="prompt"]').click(function (e) {
+
+        e.preventDefault();
+
+        bootbox.prompt($(this).data('message') || DEFAULT_PROMPT_MESSAGE, function (result) {
+
+            var match = $(e.target).data('match') || 'yes';
+
+            if (result === match) {
+                $(e.target).unbind('click')[0].click();
+            } else if (typeof result == 'string') {
+                $('.bootbox.modal')
+                    .find('.modal-dialog')
+                    .addClass('animate shake');
+
+                setTimeout(function () {
+                    $('.bootbox.modal')
+                        .find('.modal-dialog')
+                        .removeClass('shake')
+                        .removeClass('animate')
+                }, 1000);
+
+                return false;
+            }
+        });
+    });
 });
