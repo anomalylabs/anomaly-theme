a3940d25c2d2003eef0b0fdb4d0a5161bc220705
Ryan Thompson <service@aiwebsystems.com>
2015-06-24 13:55:28 -0500

    Reversed names

--- resources/js/theme/confirm.js
+++ resources/js/theme/confirm.js
@@ -1,29 +1,13 @@
 $(function () {
 
-    // Initialize prompt buttons.
-    $('[data-toggle="prompt"]').click(function (e) {
+    // Initialize confirm buttons.
+    $('[data-toggle="confirm"]').click(function (e) {
 
         e.preventDefault();
 
-        bootbox.prompt($(this).data('message') || DEFAULT_PROMPT_MESSAGE, function (result) {
-
-            var match = $(e.target).data('match') || 'yes';
-
-            if (result === match) {
+        bootbox.confirm($(this).data('message') || DEFAULT_CONFIRM_MESSAGE, function (result) {
+            if (result === true) {
                 $(e.target).unbind('click')[0].click();
-            } else if (typeof result == 'string') {
-                $('.bootbox.modal')
-                    .find('.modal-dialog')
-                    .addClass('animate shake');
-
-                setTimeout(function () {
-                    $('.bootbox.modal')
-                        .find('.modal-dialog')
-                        .removeClass('shake')
-                        .removeClass('animate')
-                }, 1000);
-
-                return false;
             }
         });
     });
--- resources/js/theme/prompt.js
+++ resources/js/theme/prompt.js
@@ -1,13 +1,29 @@
 $(function () {
 
-    // Initialize confirm buttons.
-    $('[data-toggle="confirm"]').click(function (e) {
+    // Initialize prompt buttons.
+    $('[data-toggle="prompt"]').click(function (e) {
 
         e.preventDefault();
 
-        bootbox.confirm($(this).data('message') || DEFAULT_CONFIRM_MESSAGE, function (result) {
-            if (result === true) {
+        bootbox.prompt($(this).data('message') || DEFAULT_PROMPT_MESSAGE, function (result) {
+
+            var match = $(e.target).data('match') || 'yes';
+
+            if (result === match) {
                 $(e.target).unbind('click')[0].click();
+            } else if (typeof result == 'string') {
+                $('.bootbox.modal')
+                    .find('.modal-dialog')
+                    .addClass('animate shake');
+
+                setTimeout(function () {
+                    $('.bootbox.modal')
+                        .find('.modal-dialog')
+                        .removeClass('shake')
+                        .removeClass('animate')
+                }, 1000);
+
+                return false;
             }
         });
     });
