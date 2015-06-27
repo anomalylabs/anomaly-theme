4dc5c00cd48b7ca57ef045fbfa6acd96ea13c20a
Ryan Thompson <service@aiwebsystems.com>
2015-06-27 06:10:55 -0500

    More elegant remote modal loading

--- resources/js/theme/modal.js
+++ resources/js/theme/modal.js
@@ -1,10 +1,15 @@
 $(function () {
 
+    // Toggle modals.
+    $('[data-toggle="modal"][href]').unbind().click(function (e) {
+
+        e.preventDefault();
+
+        $($(e.target).data('target')).appendTo('body').find('.modal-content').load(encodeURI($(e.target).attr('href')));
+    });
+
     // Clear remote modals when closed.
     $('.modal.remote').on('hidden.bs.modal', function () {
-
         $(this).find('.modal-content').html('<div class="modal-loading"><div class="active loader"></div></div>');
-
-        $(this).removeData('bs.modal');
     });
 });
