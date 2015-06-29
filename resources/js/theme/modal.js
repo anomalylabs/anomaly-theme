dc5762f224ad0af7447fffee8f56673b5299c651
Ryan Thompson <service@aiwebsystems.com>
2015-06-29 14:52:18 -0500

    Ya

--- resources/js/theme/modal.js
+++ resources/js/theme/modal.js
@@ -1,15 +1,10 @@
 $(function () {
 
-    // Toggle modals.
-    $('[data-toggle="modal"][href]').unbind().click(function (e) {
-
-        e.preventDefault();
-
-        $($(e.target).data('target')).appendTo('body').find('.modal-content').load(encodeURI($(e.target).attr('href')));
-    });
-
     // Clear remote modals when closed.
     $('.modal.remote').on('hidden.bs.modal', function () {
+
+        $(this).removeData('bs.modal');
+
         $(this).find('.modal-content').html('<div class="modal-loading"><div class="active loader"></div></div>');
     });
 });
