0778ec3828466f288d6bce6f9166c82731336f8a
Ryan Thompson <service@aiwebsystems.com>
2015-07-11 02:11:40 -0500

    K

---
+++ resources/js/theme/ajax.js
@@ -0,0 +1,31 @@
+$(function () {
+
+    // Detect ajax errors.
+    $(document).ajaxError(function (event, xhr, settings, exception) {
+
+        // We've been kicked out.
+        if (xhr.status == '401') {
+
+            // Close all modals.
+            $('.modal').modal('hide');
+
+            alert(exception);
+
+            // If we're in the admin redirect to admin login.
+            if (window.location.pathname.startsWith('/admin')) {
+                alert(APP_URL + '/admin/login');
+            } else {
+                alert(APP_URL + '/login');
+            }
+        }
+
+        // We're not authorized.
+        if (xhr.status == '403') {
+
+            // Close all modals.
+            $('.modal').modal('hide');
+
+            alert(exception);
+        }
+    });
+});
