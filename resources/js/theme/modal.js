c2885f9a77ab015ca9a481a16dc6375dc2d742c8
Ryan Thompson <service@aiwebsystems.com>
2015-11-18 12:55:09 -0600

    Modal work

--- resources/js/theme/modal.js
+++ resources/js/theme/modal.js
@@ -7,4 +7,18 @@ $(function () {
 
         $(this).find('.modal-content').html('<div class="modal-loading"><div class="active loader"></div></div>');
     });
+
+    // Handle ajax links in modals.
+    $('.modal').on('click', 'a.ajax', function (e) {
+
+        e.preventDefault();
+
+        var wrapper = $(this).closest('.modal-content');
+
+        wrapper.html('<div class="modal-loading"><div class="active loader"></div></div>');
+
+        $.get($(this).attr('href'), function (html) {
+            wrapper.html(html);
+        });
+    });
 });
